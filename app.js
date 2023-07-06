75abaaeacbe9ab2a156c1fa6d8ef04630759af88
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-07-06 19:06:11 +0300

    e

---
+++ app.js
@@ -0,0 +1,105 @@
+
+let currMoleTile;
+let currPlantTile; 
+let score= 0;
+let gameOver=false;
+
+window.onload = function() {
+
+    setGame();
+
+}
+
+
+function setGame () {
+
+    //set up the grid for the game board in html
+
+    for (let i=0; i<9; i++) {  //i 0'dan 8'e kadar gider ve 9'da durur.
+        //<div>0-8</div>
+        let tile=document.createElement("div")
+        tile.id =i.toString()
+        tile.addEventListener("click", selectTile)
+        document.getElementById("board").appendChild(tile);
+    }
+
+    setInterval(setMole, 1000); //1000 milisaniye = 1 saniye
+    setInterval(setPlant, 2000); //2000 milisaniye = 2 saniye
+
+}
+
+
+
+
+function getRandomTile() {
+    // math.random --> (0-1) *0 = (0-9) --> round down to (0-8) integers
+    let num= Math.floor(Math.random()*9);
+    return num.toString();
+
+}
+
+
+function setMole() {
+
+    if (gameOver) {
+        return
+    }
+
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+
+
+    let mole= document.createElement("img")
+    mole.src="images/monty-mole.png"
+
+    let num = getRandomTile()
+    if(currPlantTile && currPlantTile.id == num) {
+        return
+    } 
+    currMoleTile= document.getElementById(num)
+    currMoleTile.appendChild(mole)
+
+}
+
+function setPlant() {
+
+
+    if (gameOver) {
+        return
+    }
+
+    if (currPlantTile) {
+        
+        currPlantTile.innerHTML="";
+    }
+
+    let plant=document.createElement("img")
+    plant.src="images/piranha-plant.png"
+
+    let num = getRandomTile()
+    if(currMoleTile && currMoleTile.id == num) {
+        return
+    } 
+    currPlantTile= document.getElementById(num)
+    currPlantTile.appendChild(plant)
+
+}
+
+function selectTile() {
+
+    if (gameOver) {
+        return
+    }
+    
+    if(this==currMoleTile) {
+
+        score +=10
+        document.getElementById("score").innerText=score.toString(); // update score
+
+    } else if( this==currPlantTile) {
+        document.getElementById("score").innerText ="Oyun Bitti: " + score.toString();
+        gameOver=true;
+    }
+
+}
